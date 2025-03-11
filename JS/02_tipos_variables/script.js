62c1756daaca384e1c663c15d978fd4185f4630c
danyHernandezVVH <danyhernandez@comeduc.cl>
2025-03-11 10:57:51 -0300

    ok

---
+++ JS/02_tipos_variables/script.js
@@ -0,0 +1,18 @@
+//Creación de variables
+const rut = 20391203; //Constante
+let numero = 0; //numerica INT
+let decimal = 2.5; // num float
+let texto = "Hola"; //string
+let booleano = true; // bool
+
+let indefinida = undefined; //indefinida
+
+let array = [1,"Hola",25]; //Lista elementos
+let matriz =[[1,2],[3,4]]; //Lista dentro de lista
+let objeto = {nombre: "Dany", Edad:18}; //Objeto
+
+function sumar(a, b){ // funcion
+    return a + b
+};
+
+
